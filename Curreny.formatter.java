05a146573a502b6f0ad4e6948bae4c926cea2fb5
YuhannciaMaryS <143056982+YuhannciaMaryS@users.noreply.github.com>
2024-09-26 11:51:24 +0530

    Create Curreny.formatter.java

---
+++ Curreny.formatter.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.text.*;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = n.format(payment);
+       NumberFormat n1 = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        String india = n1.format(payment);
+        NumberFormat n2 = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = n2.format(payment);
+        NumberFormat n3 = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = n3.format(payment);
+        
+        // Write your code here.
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
